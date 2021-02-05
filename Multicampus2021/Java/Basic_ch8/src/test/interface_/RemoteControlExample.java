8b7f0dfc8a36659a25568b1013b01a1e78357438
Heyrin <beforerin@gmail.com>
2021-02-05 18:02:50 +0900

    First commit

---
+++ Multicampus2021/Java/Basic_ch8/src/test/interface_/RemoteControlExample.java
@@ -0,0 +1,19 @@
+package test.interface_;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc = null;
+		
+		rc = new Television();
+		rc.turnOn();
+		rc.setMute(true);
+		rc.turnOff();
+		
+		rc = new Audio();
+		rc.turnOn();
+		rc.turnOff();
+		
+		RemoteControl.changeBattery();
+	
+	}
+}
