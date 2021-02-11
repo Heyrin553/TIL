4aaf8d95cb448cc83093e093bce73af48dccd68e
devheyrin <beforerin@gmail.com>
2021-02-12 04:21:09 +0900

    Add AdvancedForExample.java

---
+++ Multicampus2021/Java/Basic_ch5/src/test/array/AdvancedForExample.java
@@ -0,0 +1,14 @@
+package test.array;
+
+public class AdvancedForExample {
+
+	public static void main(String[] args) {
+		String[] names = {"HJ", "YH", "JY", "CH", "CM"};
+		
+		for (String name: names) {
+			System.out.println(name);
+		}
+
+	}
+
+}
