65c5413883c5fab663d8feff33d4f740a8f23bec
devheyrin <beforerin@gmail.com>
2021-02-11 19:38:40 +0900

    Add Exercise06.java

---
+++ Multicampus2021/Java/Basic_ch3/src/test2/operator/Exercise06.java
@@ -0,0 +1,13 @@
+package test2.operator;
+
+public class Exercise06 {
+
+	public static void main(String[] args) {
+		int lenghtTop = 5;
+		int lenghBottom = 10;
+		int height = 7;
+		double area = (((double)lenghtTop + (double)lenghBottom) * (double)height / 2);
+		System.out.println(area);
+	}
+
+}
