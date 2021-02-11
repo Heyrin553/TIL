6a42743aaafef0b33e4c72a02741ba74780515af
devheyrin <beforerin@gmail.com>
2021-02-12 04:21:31 +0900

    Add ArrayCopyExample.java

---
+++ Multicampus2021/Java/Basic_ch5/src/test/array/ArrayCopyExample.java
@@ -0,0 +1,17 @@
+package test.array;
+
+public class ArrayCopyExample {
+
+	public static void main(String[] args) {
+		int[] oldIntArray = {1, 2, 3};
+		int[] newIntArray = new int[5];
+		
+		for(int i = 0; i < oldIntArray.length; i++) {
+			newIntArray[i] = oldIntArray[i];
+		}
+		for(int i = 0; i < newIntArray.length; i++) {
+			System.out.println(newIntArray[i] + ", "); // 1, 2, 3, 0, 0
+
+	}
+	}
+}
