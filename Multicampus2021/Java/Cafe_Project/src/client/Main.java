2e7251650d1329ef9652e46264006ff3905fd903
devheyrin <beforerin@gmail.com>
2021-02-17 22:37:03 +0900

    Add Cafe_project

---
+++ Multicampus2021/Java/Cafe_Project/src/client/Main.java
@@ -0,0 +1,18 @@
+package client;
+
+import client_ui.CafeUi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		CafeUi ui=new CafeUi();
+		ui.setVisible(true);
+		
+		
+		
+		
+
+	}
+
+}
