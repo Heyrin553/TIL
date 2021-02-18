6838bc33ffe12ad4552f5076ef82222b9f5dcb33
devheyrin <beforerin@gmail.com>
2021-02-18 18:30:55 +0900

    Add utility package

---
+++ Multicampus2021/Java/Cafe_Project/src/common/util/CafeException.java
@@ -0,0 +1,9 @@
+package common.util;
+
+public class CafeException extends Exception { //얘는 throw뒤에 선언해서 바로 Exception을 상속하는가?
+
+	public CafeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
