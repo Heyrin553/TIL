0434c9136a0249f6647bafac5e37b7b5431debfb
devheyrin <beforerin@gmail.com>
2021-03-16 23:23:02 +0900

    Update SemiProject

--- Multicampus2021/Servlet/tool/workspace/SemiProject/src/main/java/my/heyrin/cake/HomeController.java
+++ Multicampus2021/Servlet/tool/workspace/SemiProject/src/main/java/my/heyrin/cake/HomeController.java
@@ -1,6 +1,8 @@
 package my.heyrin.cake;
 
+import java.io.BufferedReader;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
@@ -8,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +24,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import my.heyrin.cake.service.MemberService;
+import my.heyrin.cake.service.OrderService;
 import my.heyrin.cake.util.MyException;
 import my.heyrin.cake.vo.MemberVO;
+import my.heyrin.cake.vo.OrderVO;
 
 @Controller
 public class HomeController {
@@ -35,7 +42,9 @@ public class HomeController {
 	public String login(HttpServletRequest request,
 			HttpServletResponse response){
 		String id=request.getParameter("id");
-		String pw=request.getParameter("pw");		
+		String pw=request.getParameter("pw");	
+		
+		JSONObject json=new JSONObject();
 		
 		try {
 			MemberVO m=new MemberVO(id,pw); 
@@ -43,15 +52,26 @@ public class HomeController {
 			if(name!=null) {
 				HttpSession session=request.getSession();
 				session.setAttribute("member", m);
-				return id+"님 환영합니다";
+				json.put("name", name);
 			}else {
-				return "로그인 실패";
+				json.put("msg", "로그인 실패");
 			}
 		}catch(Exception e) {
-			return e.getMessage();
-		}		
+			json.put("msg", e.getMessage());
+		}	
+		return json.toJSONString();
 	}
 	
+	@RequestMapping(value = "logout.heyrin", 
+			method= {RequestMethod.POST},
+			produces = "application/text; charset=utf8")			
+	@ResponseBody
+	public String logout(HttpServletRequest request,
+			HttpServletResponse response){
+		HttpSession session=request.getSession(false);
+		session.invalidate();
+		return "";
+	}
 
 	@RequestMapping(value = "memberInsert.heyrin", method = {
 			RequestMethod.POST }, produces = "application/text; charset=utf8") // 한글 처리
@@ -72,6 +92,58 @@ public class HomeController {
 		} catch (Exception e) {
 			return e.getMessage();
 		}
+
+	}
+	
+	@Autowired
+	OrderService orderService;
+	
+	@RequestMapping(value = "order.heyrin", method = {
+			RequestMethod.POST }, produces = "application/text; charset=utf8") // 한글 처리
+	@ResponseBody
+	public String order(HttpServletRequest request, HttpServletResponse response) {
+		JSONObject json=null;
+
+		try {
+			BufferedReader br=request.getReader();
+			JSONParser parser=new JSONParser();
+			json=(JSONObject) parser.parse(br);
+			JSONArray array=(JSONArray)json.get("product");
+			
+			Object [] array2=array.toArray();
+			
+			
+			ArrayList<OrderVO> list=new ArrayList<OrderVO>();
+			for(Object o:array2) {
+				JSONObject j=(JSONObject)o;
+				String prodName=(String)j.get("name");
+				long quantity=(Long) j.get("quantity");
+				OrderVO orderVO=new OrderVO("web", prodName, quantity);
+				HttpSession session=request.getSession();
+				MemberVO memberVO=(MemberVO)session.getAttribute("member");
+				
+				if(memberVO!=null) {
+					orderVO.setMemberId(memberVO.getId());
+				}else {
+					orderVO.setMemberId("Anonymous");
+				}
+				list.add(orderVO);
+			}
+			System.out.println("총"+list.size()+"개 품목을 주문합니다");
+			long order_group_no=orderService.orderInsert(list);
+			
+			json=new JSONObject();
+			
+			if(true) {
+				json.put("order_group_no", order_group_no);
+			}else {
+				json.put("msg", "주문 실패");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			json.put("msg", e.getMessage());
+		}
+		return json.toJSONString();
 
 	}
 
