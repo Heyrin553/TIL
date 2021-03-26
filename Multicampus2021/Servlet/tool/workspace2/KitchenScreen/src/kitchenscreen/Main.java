59706a3e5bcb96ed5e9818f66b453f2a1697d0a1
devheyrin <beforerin@gmail.com>
2021-03-26 23:10:21 +0900

    First Commit

---
+++ Multicampus2021/Servlet/tool/workspace2/KitchenScreen/src/kitchenscreen/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package kitchenscreen;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author heyrin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JFrame.setDefaultLookAndFeelDecorated(true);
+        Ui ui=new Ui();
+        ui.setBounds(300, 200, 400, 300);
+        ui.setVisible(true);
+    }
+    
+}
